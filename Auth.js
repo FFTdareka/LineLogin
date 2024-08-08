31a585d22a0c396188c15c5c23927dab9b823a73
FFT_dareka <152751544+FFTdareka@users.noreply.github.com>
2024-08-08 20:13:07 +0900

    Create Auth.js

---
+++ Auth.js
@@ -0,0 +1,25 @@
+let authCode = document.getElementById("AuthCode");
+let authButton = document.getElementById("AuthButton");
+let auth = "AbCdE";
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (location.hash != "") {
+    authCode.value = location.hash.replace("#", "");
+    authButton.click();
+  }
+})
+
+authButton.addEventListener('click', () => {
+  let authCodeText = authCode.value;
+  let errorText;
+  let state = Math.floor( Math.random() * 100000 ).toString().padStart( 5, '0');
+  let url = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2006030047&redirect_uri=https%3A%2F%2Ffftdareka.github.io%2FLineLogin%2Fcallback.html&state=" + state + "abcde&scope=profile%20openid";
+  let authExp = document.getElementById("AuthExp");
+  if (authCodeText == auth) {
+    location.href = url;
+  } else {
+    errorText = document.createElement("p");
+    errorText.innerText = "認証コードが違います。";
+    document.insertBefore(errorText, authExp);
+  }
+})
